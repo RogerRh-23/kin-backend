101b23388c1c7e399fa2e9dce622f6b1a6e966e7
Roger <rogerzapatista@gmail.com>
2026-03-11 17:02:25 -0600

    fix: Update CORS origins to include the correct production frontend URL

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ app = FastAPI(
 
 # --- 3. CONFIGURACIÓN DE CORS ---
 origins = [
-    "https://kin-frontend-production-a564.up.railway.app/auth/login",
+    "https://kin-frontend-production-a564.up.railway.app/",
+    "localhost:3000"
 ]
 
 app.add_middleware(
